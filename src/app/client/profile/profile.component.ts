49f0411aa434a20a9a3b7f37888a30f1e24e360b
kousikdas2 <kousikdasbabu2@gmail.com>
2024-07-24 06:28:37 +0530

    responsive fix

--- src/app/client/profile/profile.component.ts
+++ src/app/client/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { FooterComponent, HeaderComponent } from '@shared';
@@ -5,10 +6,18 @@ import { FooterComponent, HeaderComponent } from '@shared';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, RouterLink, RouterLinkActive, NgClass],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+ // nav button click
+ navStatus: boolean = false;
+ navBtnClick(){
+     this.navStatus = !this.navStatus;    
+ }
 
+ navBtnCloseClick(){
+   this.navStatus = false;
+ }
 }
